c3933984adcbb2e1edff8682bd99d10b29beffc2
Peter Curd <peter@pcurd.co.uk>
2013-12-27 00:27:42 +0000

    Story 2: Pacman appears on screen

--- pacmanclone.py
+++ pacmanclone.py
@@ -5,9 +5,16 @@ FPS = 30 # frames per second, the general speed of the program
 WINDOWWIDTH = 640 # size of window's width in pixels
 WINDOWHEIGHT = 480 # size of windows' height in pixels
 
+def setup():
+    global PACMAN_IMG, PACMAN_RECT
+    PACMAN_IMG = pygame.image.load('images\sprites\pacman_various_sheet.png')
+    PACMAN_RECT = pygame.Rect(398,7,29,31)
+
+
 def main():
     global FPSCLOCK, DISPLAYSURF
     pygame.init()
+    setup()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Hello World!')
@@ -16,6 +23,7 @@ def main():
       if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
        pygame.quit()
        sys.exit()
+     DISPLAYSURF.blit(PACMAN_IMG, (10,100,0,0), PACMAN_RECT)
      pygame.display.update()
      FPSCLOCK.tick(FPS)
 
